2ae4810e0616fd4666963781af3fa6f1d5d12f41
DNI9 <itsr9boi@gmail.com>
2022-07-23 22:50:44 +0530

    add Post dto

---
+++ src/main/java/com/dni9/blogapi/payload/PostDto.java
@@ -0,0 +1,11 @@
+package com.dni9.blogapi.payload;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+  private Long id;
+  private String title;
+  private String description;
+  private String content;
+}
