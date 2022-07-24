ca8cf6ff2459a7321dab072fe209ba07a7c79417
DNI9 <itsr9boi@gmail.com>
2022-07-24 18:08:16 +0530

    refactor post mapping

--- src/main/java/com/dni9/blogapi/service/impl/PostServiceImpl.java
+++ src/main/java/com/dni9/blogapi/service/impl/PostServiceImpl.java
@@ -18,18 +18,26 @@ public class PostServiceImpl implements PostService {
 
   @Override
   public PostDto createPost(PostDto data) {
+    Post post = mapToEntity(data);
+    Post newPost = postRepository.save(post);
+    return mapToDto(newPost);
+  }
+
+  private PostDto mapToDto(Post post) {
+    return PostDto.builder()
+        .id(post.getId())
+        .title(post.getTitle())
+        .content(post.getContent())
+        .description(post.getDescription())
+        .build();
+  }
+
+  private Post mapToEntity(PostDto data) {
     Post post = new Post();
     post.setTitle(data.getTitle());
     post.setDescription(data.getDescription());
     post.setContent(data.getContent());
 
-    Post newPost = postRepository.save(post);
-
-    return PostDto.builder()
-        .id(newPost.getId())
-        .title(newPost.getTitle())
-        .content(newPost.getContent())
-        .description(newPost.getDescription())
-        .build();
+    return post;
   }
 }
