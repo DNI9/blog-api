3364646260edd8858d230bfb594d64c51748a95a
DNI9 <itsr9boi@gmail.com>
2022-08-06 23:13:02 +0530

    add custom user details service

---
+++ src/main/java/com/dni9/blogapi/security/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.dni9.blogapi.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.dni9.blogapi.entity.Role;
+import com.dni9.blogapi.entity.User;
+import com.dni9.blogapi.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  public CustomUserDetailsService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+    User user = userRepository
+        .findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found with " + usernameOrEmail));
+
+    return new org.springframework.security.core.userdetails.User(
+        user.getEmail(),
+        user.getPassword(),
+        mapRolesToAuthorities(user.getRoles()));
+  }
+
+  private Collection<GrantedAuthority> mapRolesToAuthorities(Set<Role> roles) {
+    return roles.stream()
+        .map(role -> new SimpleGrantedAuthority(role.getName()))
+        .collect(Collectors.toList());
+  }
+
+}
