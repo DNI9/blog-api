eb5b044f51d6ca1c46e9afa254e472076472d0b9
DNI9 <itsr9boi@gmail.com>
2022-08-14 20:26:21 +0530

    create roles when app starts

--- src/main/java/com/dni9/blogapi/BlogApiApplication.java
+++ src/main/java/com/dni9/blogapi/BlogApiApplication.java
@@ -1,19 +1,37 @@
 package com.dni9.blogapi;
 
+import com.dni9.blogapi.entity.Role;
+import com.dni9.blogapi.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class BlogApiApplication {
+public class BlogApiApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BlogApiApplication.class, args);
-    }
+  @Autowired
+  private RoleRepository roleRepository;
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(BlogApiApplication.class, args);
+  }
+
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
+  @Override
+  public void run(String... args) {
+    Role adminRole = new Role();
+    adminRole.setName("ROLE_ADMIN");
+    roleRepository.save(adminRole);
+
+    Role userRole = new Role();
+    userRole.setName("ROLE_USER");
+    roleRepository.save(userRole);
+  }
 }
