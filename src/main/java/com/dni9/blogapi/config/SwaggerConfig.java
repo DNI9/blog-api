dd036a299299c9654a29a44865ef3711d1ab4e17
DNI9 <itsr9boi@gmail.com>
2022-08-14 18:26:27 +0530

    add global security config for swagger

--- src/main/java/com/dni9/blogapi/config/SwaggerConfig.java
+++ src/main/java/com/dni9/blogapi/config/SwaggerConfig.java
@@ -5,21 +5,32 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+  public final String SECURITY_SCHEME_NAME = "bearerAuth";
 
   @Bean
-  public OpenAPI apiInfo() {
+  public OpenAPI customizeOpenAPI() {
     Contact contact = new Contact();
     contact.setName("DNI9");
     contact.setEmail("hi@dni9.com");
     contact.setUrl("www.dni9.tech");
 
     return new OpenAPI()
-        .components(new Components())
+        .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+        .components(new Components()
+            .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                new SecurityScheme()
+                    .name(SECURITY_SCHEME_NAME)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT"))
+        )
         .info(new Info()
             .title("Blog API")
             .version("v1")
